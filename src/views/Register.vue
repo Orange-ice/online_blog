192e24b0865bc497e90eed2e16cb2f8ce5dbe7cd
Burt <2902978956@qq.com>
2020-09-04 10:32:44 +0800

    完成 Register.vue 页面布局

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,7 +1,13 @@
 <template>
-<div>
-  register 注册页面
-</div>
+  <div class="registerWrapper">
+    <h4>用户名</h4>
+    <a-input placeholder="请输入用户名" />
+    <p class="error">当前用户已存在</p>
+    <h4>密码</h4>
+    <a-input-password placeholder="请输入密码" />
+    <a-button type="primary">立即注册</a-button>
+    <p class="notice">已有账号？<router-link to="/login">直接登录</router-link></p>
+  </div>
 </template>
 
 <script lang="ts">
@@ -14,5 +20,23 @@ export default class Register extends Vue {
 </script>
 
 <style scoped lang="scss">
-
+.registerWrapper{
+  width: 450px;
+  margin: 100px auto auto;
+  h4{
+    font-size: large;
+  }
+  .error{
+    margin-top: 8px;
+    font-size: small;
+    color: #f00;
+  }
+  button{
+    margin-top: 32px;
+    margin-bottom: 42px;
+  }
+  .notice{
+    text-align: center;
+  }
+}
 </style>
