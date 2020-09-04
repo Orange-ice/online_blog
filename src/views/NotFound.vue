7fb392f03a7a81fd932413f2ecd13f4ac3138aac
Burt <2902978956@qq.com>
2020-09-04 10:37:39 +0800

    404 页面完成

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,9 @@
 <template>
-<div>404</div>
+<div>
+  <h2>你要访问的页面不存在</h2>
+  <p>点击<router-link to="/login">立即登录</router-link>
+    或者<router-link to="/register">注册账号</router-link></p>
+</div>
 </template>
 
 <script lang="ts">
@@ -12,5 +16,8 @@ export default class NotFound extends Vue {
 </script>
 
 <style scoped lang="scss">
-
+div{
+  text-align: center;
+  margin-top: 32px;
+}
 </style>
