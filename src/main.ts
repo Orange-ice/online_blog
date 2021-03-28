a6c0dcb738c9030399a7175f3412e0a57c87165f
Burt <2902978956@qq.com>
2021-03-28 13:26:57 +0800

    配置路由守卫

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/permission'
 
 import { Button,Icon,Input, message, Popover, Pagination, FormModel, Modal } from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
